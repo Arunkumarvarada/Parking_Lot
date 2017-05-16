6413a893b59fd1efa7cf03a1c114d4f36986bf61
Varada Gopal <arun-kumar.varada-gopal@hpe.com>
2017-05-16 18:40:38 +0530

    committing the code for parking lot base classes

---
+++ src/com/gojek/command/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author varadago
+ *
+ */
+package com.gojek.command;
---
+++ src/com/gojek/parkingsol/ParkingSlotManagementImpl.java
@@ -0,0 +1,30 @@
+package com.gojek.parkingsol;
+
+public class ParkingSlotManagementImpl implements ParkingSlotManagement {
+
+	public void getStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void createParkingSlots() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delteParkingSlots() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void park() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getParkingSlotsBasedonColor(String color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
